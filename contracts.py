0b6ee229321a37e4461fd56c992c6e104e73003d
Unkas2024 <unkas356@gmail.com>
2024-08-25 01:18:38 +0300

    Домашнее задание №1

---
+++ contracts.py
@@ -0,0 +1,11 @@
+USER_DATA_SCHEME = {
+    "type" : "object",
+    "properties" : {
+        "id" : {"type": "number"},
+        "name" : {"type": "string"},
+        "year" : {"type": "number"},
+        "color" : {"type": "string"},
+        "pantone_value" : {"type": "string"}
+    },
+    "required" : ["id", "name", "year", "color", "pantone_value"]
+}
