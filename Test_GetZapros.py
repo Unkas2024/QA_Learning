74f9aadcf8ebbb2c2b11284474c233d2d0cebf92
Unkas2024 <unkas356@gmail.com>
2024-08-25 17:26:22 +0300

    Добавил еще тесты

--- Test_GetZapros.py
+++ Test_GetZapros.py
@@ -9,7 +9,7 @@ Single_res = "api/unknown/2"
 Single_not = "api/unknown/23"
 Email_Ends = "@reqres.in"
 
-#Тест для list_resource
+##Тест для list_resource
 def test_list_res ():
     response = httpx.get(Base_URL + List_res)
     assert response.status_code == 200
@@ -25,8 +25,10 @@ def test_single_res ():
     assert response.status_code == 200
     print(response)
     data = response.json()['data']
+    validate (data, USER_DATA_SCHEME)
 
-#Тест для single_not
+
+##Тест для single_not
 def test_single_not ():
    response = httpx.get(Base_URL + Single_not)
    assert response.status_code == 404
