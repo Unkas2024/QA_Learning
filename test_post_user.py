ff45785f8fd32a940713279dfa46ed210173380b
Unkas2024 <unkas356@gmail.com>
2024-08-30 23:16:09 +0300

    Добавил тесты для пост и пут запросов

---
+++ test_post_user.py
@@ -0,0 +1,65 @@
+from venv import create
+
+import httpx
+from jsonschema import validate
+from core.contracts import CREATED_USER_SCHEME
+import datetime
+
+# Создаем константы c адресом ендпоинта
+Base_URL = "https://reqres.in/"
+Create_User = "api/users"
+Update_user = "api/users/2"
+
+
+# def test_create_user():
+#    body = {
+#        "name": "morpheus",
+#        "job": "leader"
+#    }
+
+#    response = httpx.post(Base_URL + Create_User, json = body)
+#    print (response.json())
+#       assert response.status_code == 201
+
+def test_update_user():
+    body = {
+        "name": "morpheus",
+        "job": "zion resident"
+    }
+
+    response = httpx.put(Base_URL + Update_user, json=body)
+#    print(response.json())
+    assert response.status_code == 200
+
+    validate(response.json(), CREATED_USER_SCHEME)
+
+    creation_date = response.json()['updatedAt'].replace('T', ' ')
+    current_date = str(datetime.datetime.utcnow())
+    assert creation_date[0:16] == current_date[0:16]
+#    print(creation_date)
+
+    assert response.json()['name'] == body['name']
+    assert response.json()['job'] == body['job']
+
+
+def test_update_all_user():
+    body = {
+        "name": "morpheus",
+        "job": "zion resident"
+    }
+
+    response = httpx.patch(Base_URL + Update_user, json=body)
+#    print(response.json())
+    assert response.status_code == 200
+
+    validate(response.json(), CREATED_USER_SCHEME)
+
+    creation_date = response.json()['updatedAt'].replace('T', ' ')
+    current_date = str(datetime.datetime.utcnow())
+    assert creation_date[0:16] == current_date[0:16]
+    assert response.json()['name'] == body['name']
+    assert response.json()['job'] == body['job']
+
+def test_delete_user():
+    response = httpx.delete(Base_URL + Update_user)
+    print(response.status_code)
